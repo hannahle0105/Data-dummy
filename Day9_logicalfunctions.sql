73d2f0ed4c764cb6511153c210886eb45f49a714
hannahle0105 <92627208+hannahle0105@users.noreply.github.com>
2024-01-06 13:18:20 +0700

    Day9_logicalfunctions.sql

--- Day9_logicalfunctions.sql
+++ Day9_logicalfunctions.sql
@@ -10,3 +10,32 @@ count (*)
 from flights
 group by mua;
 
+-- ex 1
+select 
+count(case when device_type like 'laptop' then 'laptop_views' end) as laptop_views,
+count(case when device_type in ('tablet','phone') then 'mobile_views' end) as mobile_views
+from viewership; 
+-- ex 2
+select x,y,z,
+case
+when (x + y) > z and (x + z) > y and (z + y) > x then 'Yes'
+else 'No'
+end as triangle
+from Triangle;
+-- ex 3
+select 
+round(100*
+(sum(case when call_category = 'n/a' or call_category is null then 1 else 0 end))/count(case_id)) as call_percentage
+from callers;
+-- ex 4
+select name 
+from customer 
+where referee_id <> 2 or referee_id is null;
+-- ex 5
+select 
+survived,
+sum(case when pclass = 1 then 1 else 0 end) as first_class,
+sum(case when pclass = 2 then 1 else 0 end) as second_class,
+sum(case when pclass = 3 then 1 else 0 end) as third_class
+from titanic
+group by survived;
